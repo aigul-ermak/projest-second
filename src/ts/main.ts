2dab8ab56f0b584e7a1f2974dec1c4317a18f77f
aigul <aigul.erma@gmail.com>
2022-05-07 00:27:12 -0300

    lesson 3 forms

---
+++ src/ts/main.ts
@@ -0,0 +1,16 @@
+import { modals, sliders, forms } from '../modals';
+
+window.addEventListener('DOMContentLoaded', () => {
+    modals();
+    sliders({
+        slides: '.feedback-slider-item',
+        dir: 'horizontal',
+        prev: '.main-prev-btn',
+        next: '.main-next-btn'
+    });
+    sliders({
+        slides: '.main-slider-item',
+        dir: 'vertical'
+    });
+    forms();
+});

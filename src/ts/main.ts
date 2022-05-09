9477f3237c0eaa664c4e2c79659464cf49d38409
aigul <aigul.erma@gmail.com>
2022-05-09 00:48:54 -0300

    lesson 7 calculator

---
+++ src/ts/main.ts
@@ -0,0 +1,12 @@
+import { calc } from '../modals';
+
+window.addEventListener('DOMContentLoaded', () => {
+    debugger
+    calc({
+        size: '#size',
+        material: '#material',
+        options: '#options',
+        promocode: '.promocode',
+        result: '.calc-price'
+    })
+});

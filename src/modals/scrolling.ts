16852e3f500ffd3c4478928d48160c6ca4681cf7
aigul <aigul.erma@gmail.com>
2022-05-12 20:26:20 -0300

    add types lesson-11

--- src/modals/scrolling.ts
+++ src/modals/scrolling.ts
@@ -11,100 +11,98 @@ export const scrolling = (upSelector: any) => {
         }
     });
 //second case
-    let links = document.querySelectorAll('[href^="#"]'),
-        speed = 0.3;
+//     let links = document.querySelectorAll('[href^="#"]'),
+//         speed = 0.3;
+//
+//     links.forEach(link => {
+//         link.addEventListener('click', function (event) {
+//             event.preventDefault();
+//             let widthTop = document.documentElement.scrollTop
+//             let hash = this.hash
+//             // if (!hash) return
+//             // я должна проверить hash на null
+//             // Cannot read properties of null (reading 'getBoundingClientRect')
+//             let toBlock = document.querySelector(hash).getBoundingClientRect().top,
+//                 start: any = null;
+//             requestAnimationFrame(step);
+//
+//             function step(time: number) {
+//                 if (start === null) {
+//                     start = time;
+//                 }
+//
+//                 let progress = time - start,
+//
+//                     r = (toBlock < 0
+//                         ? Math.max(widthTop - progress / speed, widthTop + toBlock)
+//                         : Math.min(widthTop + progress / speed, widthTop + toBlock));
+//
+//                 document.documentElement.scrollTo(0, r);
+//                 if (r != widthTop + toBlock) {
+//                     requestAnimationFrame(step);
+//                 } else {
+//                     location.hash = hash;
+//                 }
+//             }
+//         });
+//     });
 
-    links.forEach(link => {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-            let widthTop = document.documentElement.scrollTop
-            let hash = this.hash
-            // if (!hash) return
-            // я должна проверить hash на null
-            // Cannot read properties of null (reading 'getBoundingClientRect')
-            let toBlock = document.querySelector(hash).getBoundingClientRect().top,
-                start: any = null;
-            requestAnimationFrame(step);
+// first case
+    const element = document.documentElement,
+        body = document.body;
 
-            function step(time: any) {
-                if (start === null) {
-                    start = time;
+    const calcScroll = () => {
+        upElement.addEventListener('click', function (e: MouseEvent) {
+            let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+            if (this.hash !== '') {
+                e.preventDefault();
+
+                let hashElement = document.querySelector(this.hash),
+                    hashElementTop = 0;
+
+               // error cannot read properties of null (offsetParent)
+                if( typeof (hashElement && hashElement.offsetParent)) {
+                    while (hashElement.offsetParent) {
+                        hashElementTop += hashElement.offsetTop;
+                        hashElement = hashElement.offsetParent;
+                    }
                 }
 
-                let progress = time - start,
-
-                    r = (toBlock < 0
-                        ? Math.max(widthTop - progress / speed, widthTop + toBlock)
-                        : Math.min(widthTop + progress / speed, widthTop + toBlock));
-
-                document.documentElement.scrollTo(0, r);
-                if (r != widthTop + toBlock) {
-                    requestAnimationFrame(step);
-                } else {
-                    location.hash = hash;
-                }
+                hashElementTop = Math.round(hashElementTop);
+                smoothScroll(scrollTop, hashElementTop, this.hash)
             }
         });
-    });
+    };
 
-//first case
-    // const element = document.documentElement,
-    //     body = document.body;
-    //
-    //
-    //
-    // const calcScroll = () => {
-    //     upElement.addEventListener('click', function (e: any) {
-    //         let scrollTop = Math.round(body.scrollTop || element.scrollTop);
-    //
-    //         if (this.hash !== '') {
-    //             e.preventDefault();
-    //
-    //             let hashElement = document.querySelector(this.hash),
-    //                 hashElementTop = 0;
-    //
-    //            // error cannot read properties of null (offsetParent)
-    //             if( typeof (hashElement && hashElement.offsetParent)) {
-    //                 while (hashElement.offsetParent) {
-    //                     hashElementTop += hashElement.offsetTop;
-    //                     hashElement = hashElement.offsetParent;
-    //                 }
-    //             }
-    //
-    //             hashElementTop = Math.round(hashElementTop);
-    //             smoothScroll(scrollTop, hashElementTop, this.hash)
-    //         }
-    //     });
-    // };
-    //
-    // const smoothScroll = (from: any, to: any, hash: any) => {
-    //     let timeInterval = 1,
-    //         prevScrollTop: any,
-    //         speed: any;
-    //
-    //     if (to > from) {
-    //         speed = 30;
-    //     } else {
-    //         speed = -30
-    //     }
-    //
-    //     let move = setInterval(function () {
-    //         let scrollTop = Math.round(body.scrollTop || element.scrollTop);
-    //
-    //         if (
-    //             prevScrollTop === scrollTop ||
-    //             (to > from && scrollTop >= to) ||
-    //             (to < from && scrollTop <= to)
-    //         ) {
-    //             clearInterval(move);
-    //             history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
-    //         } else {
-    //             body.scrollTop += speed;
-    //             element.scrollTop += speed;
-    //             prevScrollTop = scrollTop;
-    //         }
-    //     }, timeInterval);
-    // };
-    // calcScroll();
+    const smoothScroll = (from: number, to: number, hash: number) => {
+        let timeInterval = 1,
+            prevScrollTop: any,
+            speed: any;
+
+        if (to > from) {
+            speed = 30;
+        } else {
+            speed = -30
+        }
+
+        let move = setInterval(function () {
+            let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+            if (
+                prevScrollTop === scrollTop ||
+                (to > from && scrollTop >= to) ||
+                (to < from && scrollTop <= to)
+            ) {
+                clearInterval(move);
+                history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
+            } else {
+                body.scrollTop += speed;
+                element.scrollTop += speed;
+                prevScrollTop = scrollTop;
+            }
+        }, timeInterval);
+    };
+    calcScroll();
 };
 
